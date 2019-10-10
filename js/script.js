3d9794498c562d70c830e38fdf6d9f475d0936bc
Marcin Kubiak <mkubiak73@gmail.com>
2019-10-10 18:15:28 +0200

    add titleClickHandler script

---
+++ js/script.js
@@ -0,0 +1,23 @@
+{
+  const titleClickHandler = function(event) {
+    console.log(event.target);
+
+    /* remove class 'active' from all article links  */
+
+    /* add class 'active' to the clicked link */
+
+    /* remove class 'active' from all articles */
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+  };
+
+  const links = document.querySelectorAll('.titles a');
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
+}
