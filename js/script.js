6fe5e1ad51e160b710080abdfba9601dd059472f
Marcin Kubiak <mkubiak73@gmail.com>
2019-10-10 18:26:01 +0200

    add remove class active from articles & articles links

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@
     console.log(event.target);
 
     /* remove class 'active' from all article links  */
-
+    let activeLinks = document.querySelectorAll('.titles a.active');
+    for (const activLink of activeLinks) {
+      activLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
-
+    let activeArticles = document.querySelectorAll('article.active');
+    for (const activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
