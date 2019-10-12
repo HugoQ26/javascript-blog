15879767bc76e48b3631325ee056d7377ccd6d4a
Marcin Kubiak <mkubiak73@gmail.com>
2019-10-12 20:06:31 +0200

    add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -30,9 +30,56 @@
     correctArticle.classList.add('active');
   };
 
-  const links = document.querySelectorAll('.titles a');
+  /*************************************************************/
 
-  for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-  }
+  let html = '';
+
+  const generateTitleLinks = (function() {
+    //usuń zawartość listy linków w lewej kolumnie,
+    /* [DONE}] remove list links content from left column */
+    const allLinks = document.querySelector('ul.list.titles');
+    allLinks.innerHTML = '';
+
+    /* [DONE] find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll('article');
+
+    for (const article of articles) {
+      /* [DONE] get the article id and save it to constant */
+      const articleId = article.getAttribute('id');
+
+      /* [DONE] find element with article title and and save it to constant */
+      const articleTitle = article.querySelector('.post-title').innerHTML;
+
+      /* [DONE] create HTML of the link */
+
+      const linkHtml =
+        '<li><a href=#' +
+        articleId +
+        '><span>' +
+        articleTitle +
+        '</span></a></li>';
+      /* [DONE] insert link into html variable */
+      html += linkHtml;
+
+      //   const nodeLi = document.createElement('li');
+      //   const nodeA = document.createElement('a');
+      //   const nodeSpan = document.createElement('span');
+
+      //   allLinks
+      //     .appendChild(nodeLi)
+      //     .appendChild(nodeA)
+      //     .appendChild(nodeSpan).innerHTML = articleTitle;
+
+      //   const d = article
+      //     .getElementsByTagName('a')
+      //     .setAttribute('href', articleId);
+    }
+    allLinks.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
+  })();
 }
